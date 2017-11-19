84c6d5d563557c12ef8b0a4d0747a8f4c02bd18e
lachaka <lachomldenov@gmail.com>
2017-11-19 20:56:43 +0200

    Modify code for negative images generator. Added more samples used for generating.

--- datasets/neg_images/img_gen.py
+++ datasets/neg_images/img_gen.py
@@ -5,12 +5,11 @@ import skimage.io
 directory = './data/'
 images_path = './samples/'
 bg_file = open('bg.txt', 'w')
-
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 name = 0;
-img_names = ['0.ppm', '00199.ppm', '00215.ppm', '00273.ppm']
+img_names = [f for f in os.listdir(images_path) if f.endswith(".ppm")]
 
 for img_name in img_names:
 	img = cv2.imread(images_path + img_name)
@@ -21,6 +20,6 @@ for img_name in img_names:
 			crop_img = img[j:(j + 128), i:(i + 128)]
 			img_full_path = directory + str(name) + '.ppm'
 			cv2.imwrite(img_full_path, crop_img)
-			bg_file.write(img_full_path + '\n')
+			bg_file.write('./datasets/neg_images/data/' + str(name) + '.ppm' + '\n')
 			name += 1
 bg_file.close()
