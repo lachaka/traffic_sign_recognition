c0aaf20bf732cd7b77e288ca1052c667b698e743
lachaka <lachomldenov@gmail.com>
2017-12-03 21:47:35 +0200

    Modifying photos generator and fixing some issues.

--- datasets/neg_images/img_gen.py
+++ datasets/neg_images/img_gen.py
@@ -2,14 +2,15 @@ import cv2
 import os
 import skimage.io
 
-directory = './data/'
+output_dir = '../training_images/Final_Training/Images/00043/'
 images_path = './samples/'
-bg_file = open('bg.txt', 'w')
-if not os.path.exists(directory):
-    os.makedirs(directory)
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 name = 0;
-img_names = [f for f in os.listdir(images_path) if f.endswith(".ppm")]
+img_names = [f for f in os.listdir(images_path)
+				if f.endswith(".ppm")]
 
 for img_name in img_names:
 	img = cv2.imread(images_path + img_name)
@@ -17,9 +18,7 @@ for img_name in img_names:
 
 	for i in range(0, w - 128, 32):
 		for j in range(0, h - 128, 32):
-			crop_img = img[j:(j + 128), i:(i + 128)]
-			img_full_path = directory + str(name) + '.ppm'
+			crop_img = img[j : (j + 128), i : (i + 128)]
+			img_full_path = output_dir + '0_' + str(name) + '.ppm'
 			cv2.imwrite(img_full_path, crop_img)
-			bg_file.write('./datasets/neg_images/data/' + str(name) + '.ppm' + '\n')
 			name += 1
-bg_file.close()
