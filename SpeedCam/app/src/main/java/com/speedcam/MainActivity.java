6b395053ce962ef3a59261f3c74a09d69183ff4a
lachaka <lachomldenov@gmail.com>
2018-03-05 18:44:22 +0200

    Added Activity asking for permissions and changed the main logic.

--- SpeedCam/app/src/main/java/com/speedcam/MainActivity.java
+++ SpeedCam/app/src/main/java/com/speedcam/MainActivity.java
@@ -1,212 +1,104 @@
 package com.speedcam;
 
-
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceView;
-import android.view.WindowManager;
-import android.widget.ImageView;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
-import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.concurrent.ExecutionException;
-
-import static com.speedcam.Constants.CASCADE_FILE_PATH;
-import static com.speedcam.Constants.COLOR_CHANNELS;
-import static com.speedcam.Constants.IMAGE_HEIGHT;
-import static com.speedcam.Constants.IMAGE_WIDTH;
-import static com.speedcam.Constants.INPUT_NODE;
-import static com.speedcam.Constants.OUTPUT_NODE;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-        implements CameraBridgeViewBase.CvCameraViewListener2 {
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private static final String TAG = MainActivity.class.getName();
+    public static final String TAG = "MainActivity";
 
-    private LoadSignClassifier signClassifier;
-
-    private ImageView signView;
-    private JavaCameraView cameraView;
-    private Mat frame;
-
-    private HashMap<Long, Integer> signImages;
-
-    BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            if (status == BaseLoaderCallback.SUCCESS) {
-                cameraView.enableView();
-            } else {
-                super.onManagerConnected(status);
-            }
-        }
-    };
+    private static final int REQUEST_PERMISSION_CODE = 0;
+//    private static final int REQUEST_CAMERA = 1;
+//    private static final int REQUEST_ACCESS_FINE_LOCATION = 2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        initSignImages();
-
-        signClassifier = new LoadSignClassifier(getAssets());
-        signView = findViewById(R.id.sign_view);
-        cameraView = findViewById(R.id.camera_view);
-        cameraView.setVisibility(SurfaceView.VISIBLE);
-        cameraView.setMaxFrameSize(320, 240);
-        cameraView.setCvCameraViewListener(this);
-
-    }
-
-    private void initSignImages() {
-        signImages = new HashMap<>();
-
-        long count = 0;
-        for (Field field : R.drawable.class.getFields()) {
-            if (field.getName().contains("sign_number")) {
-                try {
-                    field.setAccessible(true);
-                    signImages.put(count++, field.getInt(R.drawable.class));
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (cameraView != null) {
-            cameraView.disableView();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (OpenCVLoader.initDebug()) {
-            loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        if (checkIfAlreadyHavePermissions()) {
+            Toast.makeText(MainActivity.this,
+                    "You have already granted the necessary permissions!", Toast.LENGTH_SHORT).show();
+            switchActivity();
         } else {
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0,
-                    this, loaderCallback);
+//            requestLocationPermission()
+//            requestCameraPermission();
+            ActivityCompat.requestPermissions(this, 
+                    new String[] {Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSION_CODE);
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        cameraView.disableView();
+    private boolean checkIfAlreadyHavePermissions() {
+        int cameraPermission = ContextCompat
+                .checkSelfPermission(this, Manifest.permission.CAMERA);
+        int locationPermission = ContextCompat
+                .checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        return cameraPermission == PackageManager.PERMISSION_GRANTED &&
+                locationPermission == PackageManager.PERMISSION_GRANTED;
     }
 
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        frame = new Mat(height, width, CvType.CV_32FC3);
-    }
+//    private void requestCameraPermission() {
+//        ActivityCompat.requestPermissions(this,
+//                new String[] {Manifest.permission.CAMERA}, REQUEST_CAMERA);
+//
+//    }
+//
+//    private void requestLocationPermission() {
+//        ActivityCompat.requestPermissions(this,
+//                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
+//    }
 
     @Override
-    public void onCameraViewStopped() {
-        frame.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        frame = inputFrame.rgba();
-
-        MatOfRect detectedObjects = new MatOfRect();
-        Mat frameGray = new Mat();
-        Imgproc.cvtColor(frame, frameGray, Imgproc.COLOR_BGRA2GRAY);
-        CascadeClassifier cascadeDetector = new CascadeClassifier(CASCADE_FILE_PATH);
-
-        cascadeDetector.detectMultiScale(frameGray, detectedObjects);
-
-        for (Rect object : detectedObjects.toArray()) {
-            Rect objectCoordinates = new Rect(object.x, object.y, object.width, object.height);
-            Mat croppedObject = new Mat(frame, objectCoordinates);
-
-            Mat resizedImg = new Mat(IMAGE_WIDTH, IMAGE_HEIGHT, CvType.CV_32FC4);
-            Imgproc.resize(croppedObject, resizedImg, new Size(IMAGE_WIDTH, IMAGE_HEIGHT));
-
-            float[] floatArrImg = convertMatToFloatArray(resizedImg);
-            try {
-                long signNumber = (long) new SignRecognition().execute(signClassifier.getTensorFlow(),
-                        floatArrImg).get();
-                drawRectangle(object, signNumber);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permissions have now been granted.");
+                    switchActivity();
+                } else {
+                    Log.i(TAG, "Permissions have now been DENIED.");
+                    finishAndRemoveTask ();
+                }
+                return;
             }
-        }
-        return frame;
-    }
-
-    private float[] convertMatToFloatArray(Mat image) {
-        Bitmap bitmap = Bitmap.createBitmap(image.cols(),image.rows(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(image, bitmap);
-
-        int[] intValues = new int[32 * 32];
-        float[] result = new float[32 * 32 * 3];
-
-        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0,
-                bitmap.getWidth(), bitmap.getHeight());
-        for (int i = 0; i < intValues.length; ++i) {
-            int tmp = intValues[i];
-            result[i * 3 + 0] = ((tmp >> 16) & 0xFF) / 255.0F;
-            result[i * 3 + 1] = ((tmp >> 8) & 0xFF) / 255.0F;
-            result[i * 3 + 2] = (tmp & 0xFF) / 255.0F;
-        }
-        return result;
-    }
-
-    private void drawRectangle(Rect sign, long signNumber) {
-        if (signNumber != 43) {
-            Imgproc.rectangle(frame, sign.tl(), sign.br(),
-                    new Scalar(255, 0, 255), 1);
+//            case REQUEST_CAMERA: {
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Log.i(TAG, "CAMERA permission has now been granted.");
+//                    switchActivity();
+//                } else {
+//                    Log.i(TAG, "CAMERA permission has now been DENIED.");
+//                    finishAndRemoveTask ();
+//                }
+//                return;
+//            }
+//
+//            case REQUEST_ACCESS_FINE_LOCATION: {
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    Log.i(TAG, "LOCATION permission has now been granted.");
+//                    startActivity(new Intent(this, HomeActivity.class));
+//                } else {
+//                    Log.i(TAG, "LOCATION permission has now been DENIED.");
+//                    finishAndRemoveTask ();
+//                }
+//                return;
+//            }
         }
     }
 
-    public class SignRecognition extends AsyncTask<Object, Void, Long> {
-
-        @Override
-        protected Long doInBackground(Object... objects) {
-            TensorFlowInferenceInterface tfInterface = (TensorFlowInferenceInterface) objects[0];
-            float[] inputData = (float[]) objects[1];
-
-            long[] outputResult = {0, 0};
-            tfInterface.feed(INPUT_NODE, inputData, 1,
-                    IMAGE_WIDTH, IMAGE_HEIGHT, COLOR_CHANNELS);
-            tfInterface.run(new String[]{OUTPUT_NODE}, false);
-            tfInterface.fetch("prediction", outputResult);
-
-            return outputResult[0];
-        }
-
-        @Override
-        protected void onPostExecute(Long signNumber) {
-            if (signNumber < 8) { //TODO: add all signs
-                signView.setImageResource(signImages.get(signNumber));
-            }
-        }
+    private void switchActivity() {
+        startActivity(new Intent(this, HomeActivity.class));
     }
 }
--- SpeedCam/app/src/main/java/com/speedcam/MeasureSpeed.java
+++
@@ -1,6 +0,0 @@
-package com.speedcam;
-
-
-
-public class MeasureSpeed {
-}
