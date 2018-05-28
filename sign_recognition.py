8f78d810d0a6703cd2e496baef056e9cf6df1341
lachaka <lachomldenov@gmail.com>
2018-05-28 18:17:47 +0300

    Fixed some issus with color channels. Uploaded new frozen model.

--- sign_recognition.py
+++ sign_recognition.py
@@ -4,7 +4,6 @@
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import skimage.transform
 from skimage import io
 import tensorflow as tf
@@ -26,14 +25,14 @@ saver = tf.train.import_meta_graph('./model/model.meta')
 sess = tf.Session()
 saver.restore(sess, tf.train.latest_checkpoint('./model/'))
 
+
 while (video.isOpened()):
     ret, frame = video.read()
     if not ret:
         break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    signs = sign_cascade.detectMultiScale(gray, 1.3, 5)
- 
 
+    signs = sign_cascade.detectMultiScale(frame, 1.4, 5)
+ 
     for (x, y, w, h) in signs:
         cropped_sign = frame[y: y + h, x: x + w]
 
@@ -46,10 +45,8 @@ while (video.isOpened()):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(frame, str(predicted), (x, y - 10), font, 0.5, (255, 0, 255), 2, cv2.LINE_AA)
-    
-    cv2.imshow('frame', frame)
+   
     out.write(frame)
 
 video.release()
 out.release()
-cv2.destroyAllWindows()
